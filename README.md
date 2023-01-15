d2ee0658752f6f68cea47b5379837442aa0beb85
puripuri2100 <puripuri2100@gmail.com>
2023-01-16 00:38:11 +0900

    add README and LICENSE

---
+++ README.md
@@ -0,0 +1,16 @@
+# ocr_precedent
+
+判例のPDFをOCRでテキストに変換するプログラム
+
+requires:
+- [tesseract](https://github.com/tesseract-ocr/tesseract)
+- tesseract-ocr-jpn
+- ImageMagick
+- poppler-utils
+
+---
+[MIT License](https://github.com/japanese-law-analysis/ocr_precedent/blob/master/LICENSE)
+(c) 2021 Naoki Kaneko (a.k.a. "puripuri2100")
+
+
+License: MIT
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+//! 判例のPDFをOCRでテキストに変換するプログラム
+//!
 //! requires:
 //! - [tesseract](https://github.com/tesseract-ocr/tesseract)
 //! - tesseract-ocr-jpn
 //! - ImageMagick
 //! - poppler-utils
 //!
+//! ---
+//! [MIT License](https://github.com/japanese-law-analysis/ocr_precedent/blob/master/LICENSE)
+//! (c) 2021 Naoki Kaneko (a.k.a. "puripuri2100")
+//!
+
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use regex::Regex;
